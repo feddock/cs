86ed010e2f8d5c318c91ab9ac2088b2e207dcacb
unknown <hdsturgill@rarity.wcu.edu>
2015-11-08 11:19:09 -0500

    Messed up the project on importing, had to rebuild

--- assignment3/src/LsetTest.java
+++ assignment3/src/LsetTest.java
@@ -6,34 +6,29 @@ public class LsetTest {
 
 	@Test
 	public void testAdd() {
-		fail("Not yet implemented");
+		fail("Not yet implemented"); // TODO
 	}
 
 	@Test
 	public void testClear() {
 		Lset<String> set = new Lset<>();
-		
 		set.add("One Element");
-		set.clear();
-		
-		assertEquals("Cleared list size should be zero", 0, set.size());
+		set.clear();		
 		assertTrue("Cleared list should be empty", set.isEmpty());
+		assertEquals("Size of cleared list is zero", 0, set.size());
 	}
 
 	@Test
 	public void testIsEmpty() {
 		Lset<String> set = new Lset<>();
-		
-		assertTrue("New set should be empty", set.isEmpty());
-		
-		set.add("One Element");
-		
-		assertFalse("One element set should not be empty", set.isEmpty());
+		assertTrue("New list should be empty", set.isEmpty());	
+		set.add("One element");	
+		assertFalse("List with item should not be empty", set.isEmpty());
 	}
 
 	@Test
 	public void testSize() {
-		fail("Not yet implemented");
+		fail("Not yet implemented"); // TODO
 	}
 
 }
